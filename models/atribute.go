95a5cab567e100ed9571226d2dd364eebdd8d491
Byron Villegas Moya <byronvillegasm@gmail.com>
2025-04-29 21:14:50 -0400

    Rename file

--- models/atribute.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Attribute struct {
-	Element      string `json:"element"`
-	Rarity       string `json:"rarity"`
-	ManaCost     int    `json:"manaCost"`
-	Color        string `json:"color"`
-	TypeSpecific string `json:"typeSpecific"`
-}
