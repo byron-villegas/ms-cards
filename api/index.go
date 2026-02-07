85350345bfad988a45dd558bfe67a8c90d67c579
Byron Villegas Moya <byronvillegasm@gmail.com>
2026-02-07 15:43:16 -0300

    Changed index

--- api/index.go
+++ api/index.go
@@ -3,20 +3,24 @@ package main
 import (
 	"ms-cards/config"
 	"ms-cards/routes"
+	"net/http"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+var router *gin.Engine
+
+func init() {
 	// Show banner with application information
 	config.ShowBanner()
 
 	// Create a server instance
-	r := gin.Default()
+	router = gin.Default()
 
 	// Config CORS
-	r.Use(cors.New(cors.Config{
+	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{config.AppConfig.CORSOrigin},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{config.AppConfig.CORSHeaders},
@@ -24,11 +28,13 @@ func main() {
 	}))
 
 	// Config server path
-	routerGroup := r.Group(config.AppConfig.ServerPath)
+	routerGroup := router.Group(config.AppConfig.ServerPath)
 
 	// Config routes for the server
 	routes.SetupRoutes(routerGroup)
+}
 
-	// Start server with port
-	r.Run(":" + config.AppConfig.ServerPort)
+// Handler is the entry point for Vercel serverless function
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router.ServeHTTP(w, r)
 }
